a792db1dec5f6095925b384b55c8e9bb818e9e00
Pema-sDaddy <toddfhughes@yahoo.com>
2016-06-28 14:55:29 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -89,7 +89,7 @@ $(function() {
 
 	// list views from Cloudant that we want to offer as layers
 	var cloudantViews = [];
-	$.getJSON('http://pema-sdaddy.github.io/mapping-berlin/',
+	$.getJSON('https://garuda3060.cloudant.com/boston/',
 		function(result) {
 			var viewsList = result.views;
 			for (var v in viewsList) {
@@ -122,7 +122,7 @@ $("#search").submit(function(event) {
 
 function getLayer(callback, cloudantView) {
 	var cloudantURLbase =
-		"http://pema-sdaddy.github.io/mapping-berlin/";
+		"https://garuda3060.cloudant.com/boston/_design/tour/_view/";
 	var cloudantURLcallback = "?callback=?";
 	var thisCloudantURL = cloudantURLbase + cloudantView + cloudantURLcallback;
 	$.getJSON(thisCloudantURL, function(result) {
@@ -138,7 +138,7 @@ function getLayer(callback, cloudantView) {
 // See http://stackoverflow.com/questions/19916894/wait-for-multiple-getjson-calls-to-finish
 function searchPoints(callback, cloudantSearch) {
 	var cloudantURLbase =
-		"http://pema-sdaddy.github.io/mapping-berlin/ids?q=";
+		"https://garuda3060.cloudant.com/boston/_design/tour/_search/ids?q=";
 	var cloudantURLcallback = "&callback=?";
 	var thisCloudantURL = cloudantURLbase + cloudantSearch + cloudantURLcallback;
 	$.getJSON(thisCloudantURL, function(result) {
@@ -157,7 +157,7 @@ function getPoints(cloudantIDs) {
 	}
 
 	function getPoint(id) {
-		var cloudantURLbase = "http://pema-sdaddy.github.io/mapping-berlin/";
+		var cloudantURLbase = "https://garuda3060.cloudant.com/boston/";
 		var url = cloudantURLbase + id;
 		return $.getJSON(url); // this returns a "promise"
 	}
